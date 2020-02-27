43384079811bb79e43592b707a74654c9885c792
Victor Jimenez <vic.jicama@gmail.com>
2020-02-27 18:24:47 +0100

    Sync to latest version

--- start.sh
+++ start.sh
@@ -6,10 +6,10 @@ export LOCAL_DEVICE_ID=$1
 export LOCAL_TARGET_SERVER_PORT=25000
 export LOCAL_WORKSPACE=./
 
-export LOCAL_VERSION_BOOTSTRAP=1.70.17-initial-client-prod #module @nebulario/tunnel-local-boot-graph
-export LOCAL_VERSION_GRAPH=1.70.11-initial-client-prod #module repoflow/tunnel-local-graph-container
-export LOCAL_VERSION_WEB=1.70.8-initial-client-prod #module repoflow/tunnel-local-web-container
-export LOCAL_VERSION_WORKER=1.70.4-initial-client-prod #module repoflow/tunnel-local-worker-graph-container
+export LOCAL_VERSION_BOOTSTRAP=1.70.0-master #module @nebulario/tunnel-local-boot-graph
+export LOCAL_VERSION_GRAPH=1.70.0-master #module repoflow/tunnel-local-graph-container
+export LOCAL_VERSION_WEB=1.70.0-master #module repoflow/tunnel-local-web-container
+export LOCAL_VERSION_WORKER=1.70.0-master #module repoflow/tunnel-local-worker-graph-container
 
 echo "Starting..."
 echo "{\"dependencies\":{\"@nebulario/tunnel-local-boot-graph\":\"${LOCAL_VERSION_BOOTSTRAP}\"}}" > package.json
